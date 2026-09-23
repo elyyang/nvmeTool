6eacd74f2d79c076d718179f744c723b8c8cdc68
Eric L Yang <elyyang@gmail.com>
2026-09-22 21:21:11 -0700

    add volatile qualifier

--- src/include/pcieConfigHeader_g4.h
+++ src/include/pcieConfigHeader_g4.h
@@ -45,47 +45,47 @@
 PCIe config space header (type 0)
 **************************************************************/
 
-#define PCIE_CONFIG_SPACE_HEADER_OFFSET_VENDOR_ID             (0x0)
-#define PCIE_CONFIG_SPACE_HEADER_OFFSET_DEVICE_ID             (0x2)
-#define PCIE_CONFIG_SPACE_HEADER_OFFSET_COMMAND               (0x4)
-#define PCIE_CONFIG_SPACE_HEADER_OFFSET_STATUS                (0x6)
-#define PCIE_CONFIG_SPACE_HEADER_OFFSET_REV_ID                (0x8)
-#define PCIE_CONFIG_SPACE_HEADER_OFFSET_CLASS_CODE            (0x9)
-#define PCIE_CONFIG_SPACE_HEADER_OFFSET_CACHE_LINE_SIZE       (0xC)
-#define PCIE_CONFIG_SPACE_HEADER_OFFSET_LATENCY_TIMER         (0xD)
-#define PCIE_CONFIG_SPACE_HEADER_OFFSET_HEADER_TYPE           (0xE)
-#define PCIE_CONFIG_SPACE_HEADER_OFFSET_BIST                  (0xF)
-#define PCIE_CONFIG_SPACE_HEADER_OFFSET_BAR0                  (0x10)
-#define PCIE_CONFIG_SPACE_HEADER_OFFSET_BAR1                  (0x14)
-#define PCIE_CONFIG_SPACE_HEADER_OFFSET_BAR2                  (0x18)
-#define PCIE_CONFIG_SPACE_HEADER_OFFSET_BAR3                  (0x1C)
-#define PCIE_CONFIG_SPACE_HEADER_OFFSET_BAR4                  (0x20)
-#define PCIE_CONFIG_SPACE_HEADER_OFFSET_BAR5                  (0x24)
-#define PCIE_CONFIG_SPACE_HEADER_OFFSET_CARDBUS_CIS_PTR       (0x28)
-#define PCIE_CONFIG_SPACE_HEADER_OFFSET_SUBSYSTEM_VENDOR_ID   (0x2C)
-#define PCIE_CONFIG_SPACE_HEADER_OFFSET_SUBSYSTEM_ID          (0x2E)
-#define PCIE_CONFIG_SPACE_HEADER_OFFSET_EXPANSION_ROM         (0x30)
-#define PCIE_CONFIG_SPACE_HEADER_OFFSET_CAP_PTR               (0x34)
-#define PCIE_CONFIG_SPACE_HEADER_OFFSET_INT_LINE              (0x3C)
-#define PCIE_CONFIG_SPACE_HEADER_OFFSET_INT_PIN               (0x3D)
-#define PCIE_CONFIG_SPACE_HEADER_OFFSET_MIN_GNT               (0x3E)
-#define PCIE_CONFIG_SPACE_HEADER_OFFSET_MAX_LAT               (0x3F)
+#define PCIE_CONFIG_SPACE_HEADER_OFFSET_VENDOR_ID                   (0x0)
+#define PCIE_CONFIG_SPACE_HEADER_OFFSET_DEVICE_ID                   (0x2)
+#define PCIE_CONFIG_SPACE_HEADER_OFFSET_COMMAND                     (0x4)
+#define PCIE_CONFIG_SPACE_HEADER_OFFSET_STATUS                      (0x6)
+#define PCIE_CONFIG_SPACE_HEADER_OFFSET_REV_ID                      (0x8)
+#define PCIE_CONFIG_SPACE_HEADER_OFFSET_CLASS_CODE                  (0x9)
+#define PCIE_CONFIG_SPACE_HEADER_OFFSET_CACHE_LINE_SIZE             (0xC)
+#define PCIE_CONFIG_SPACE_HEADER_OFFSET_LATENCY_TIMER               (0xD)
+#define PCIE_CONFIG_SPACE_HEADER_OFFSET_HEADER_TYPE                 (0xE)
+#define PCIE_CONFIG_SPACE_HEADER_OFFSET_BIST                        (0xF)
+#define PCIE_CONFIG_SPACE_HEADER_OFFSET_BAR0                        (0x10)
+#define PCIE_CONFIG_SPACE_HEADER_OFFSET_BAR1                        (0x14)
+#define PCIE_CONFIG_SPACE_HEADER_OFFSET_BAR2                        (0x18)
+#define PCIE_CONFIG_SPACE_HEADER_OFFSET_BAR3                        (0x1C)
+#define PCIE_CONFIG_SPACE_HEADER_OFFSET_BAR4                        (0x20)
+#define PCIE_CONFIG_SPACE_HEADER_OFFSET_BAR5                        (0x24)
+#define PCIE_CONFIG_SPACE_HEADER_OFFSET_CARDBUS_CIS_PTR             (0x28)
+#define PCIE_CONFIG_SPACE_HEADER_OFFSET_SUBSYSTEM_VENDOR_ID         (0x2C)
+#define PCIE_CONFIG_SPACE_HEADER_OFFSET_SUBSYSTEM_ID                (0x2E)
+#define PCIE_CONFIG_SPACE_HEADER_OFFSET_EXPANSION_ROM               (0x30)
+#define PCIE_CONFIG_SPACE_HEADER_OFFSET_CAP_PTR                     (0x34)
+#define PCIE_CONFIG_SPACE_HEADER_OFFSET_INT_LINE                    (0x3C)
+#define PCIE_CONFIG_SPACE_HEADER_OFFSET_INT_PIN                     (0x3D)
+#define PCIE_CONFIG_SPACE_HEADER_OFFSET_MIN_GNT                     (0x3E)
+#define PCIE_CONFIG_SPACE_HEADER_OFFSET_MAX_LAT                     (0x3F)
 
-#define PCIE_CONFIG_SPACE_HEADER_SIZE                         (0x40)
+#define PCIE_CONFIG_SPACE_HEADER_SIZE                               (0x40)
 
 /**************************************************************
 PCIe header status bit
 **************************************************************/
 
-#define PCIE_CONFIG_SPACE_HEADER_STATUS_IMMEDIATE_READINESS          (0x1)
-#define PCIE_CONFIG_SPACE_HEADER_STATUS_INTERRUPT_STATUS             (0x8)
-#define PCIE_CONFIG_SPACE_HEADER_STATUS_CAPABILITIES_LIST            (0x10)
-#define PCIE_CONFIG_SPACE_HEADER_STATUS_MASTER_DATA_PARITY_ERROR     (0x100)
-#define PCIE_CONFIG_SPACE_HEADER_STATUS_SIGNALED_TARGET_ABORT        (0x800)
-#define PCIE_CONFIG_SPACE_HEADER_STATUS_RECEIVED_TARGET_ABORT        (0x1000)
-#define PCIE_CONFIG_SPACE_HEADER_STATUS_RECEIVED_MASTER_ABORT        (0x2000)
-#define PCIE_CONFIG_SPACE_HEADER_STATUS_SIGNALED_SYSTEM_ERROR        (0x4000)
-#define PCIE_CONFIG_SPACE_HEADER_STATUS_DETECTED_PARITY_ERROR        (0x8000)
+#define PCIE_CONFIG_SPACE_HEADER_STATUS_IMMEDIATE_READINESS         (0x1)
+#define PCIE_CONFIG_SPACE_HEADER_STATUS_INTERRUPT_STATUS            (0x8)
+#define PCIE_CONFIG_SPACE_HEADER_STATUS_CAPABILITIES_LIST           (0x10)
+#define PCIE_CONFIG_SPACE_HEADER_STATUS_MASTER_DATA_PARITY_ERROR    (0x100)
+#define PCIE_CONFIG_SPACE_HEADER_STATUS_SIGNALED_TARGET_ABORT       (0x800)
+#define PCIE_CONFIG_SPACE_HEADER_STATUS_RECEIVED_TARGET_ABORT       (0x1000)
+#define PCIE_CONFIG_SPACE_HEADER_STATUS_RECEIVED_MASTER_ABORT       (0x2000)
+#define PCIE_CONFIG_SPACE_HEADER_STATUS_SIGNALED_SYSTEM_ERROR       (0x4000)
+#define PCIE_CONFIG_SPACE_HEADER_STATUS_DETECTED_PARITY_ERROR       (0x8000)
 
 /**************************************************************
 PCIe header class code
@@ -158,19 +158,19 @@ typedef struct __attribute__((packed, aligned (4))) pcieConfigurationHeader_t
     //PCIe config header Dword 0 (byte offset 00h)
     union
     {
-        struct
+        volatile struct
         {
             uint32_t vendorId :16;    
             uint32_t deviceId :16;    
         };
 
-        uint32_t all;
+        volatile uint32_t all;
     }dw0;
 
     //PCIe config header Dword 1 (byte offset 04h)
     union
     {
-        struct
+        volatile struct
         {
             commandReg_t command;                           
             statusReg_t status;                         
@@ -182,19 +182,19 @@ typedef struct __attribute__((packed, aligned (4))) pcieConfigurationHeader_t
     //PCIe config header Dword 2 (byte offset 08h)
     union
     {
-        struct
+        volatile struct
         {
             uint32_t revId     :8;     
             uint32_t classCode :24;    
         };
 
-        uint32_t all;
+        volatile uint32_t all;
     }dw2;
 
     //PCIe config header Dword 3 (byte offset 0Ch)
     union
     {
-        struct
+        volatile struct
         {
             uint32_t cacheLineSize :8;     
             uint32_t latencyTimer  :8;
@@ -202,20 +202,20 @@ typedef struct __attribute__((packed, aligned (4))) pcieConfigurationHeader_t
             uint32_t bist          :8;
         };
 
-        uint32_t all;
+        volatile uint32_t all;
     }dw3;
 
     //PCIe config header Dword 4 (byte offset 10h)
-    uint32_t bar0;
+    volatile uint32_t bar0;
     
     //PCIe config header Dword 5 (byte offset 14h)
-    uint32_t bar1;
+    volatile uint32_t bar1;
     
     //PCIe config header Dword 6 (byte offset 18h)
     union
     {
         //type 1 config space header specific
-        struct 
+        volatile struct 
         {
             uint32_t primaryBusNumber       :8;
             uint32_t secondaryBusNumber     :8;
@@ -224,16 +224,16 @@ typedef struct __attribute__((packed, aligned (4))) pcieConfigurationHeader_t
         };
 
         //type 0 config space header specific
-        uint32_t bar2;
+        volatile uint32_t bar2;
 
-        uint32_t all;
+        volatile uint32_t all;
     }dw6;
     
     //PCIe config header Dword 7 (byte offset 1Ch)
     union
     {
         //type 1 config space header specific
-        struct 
+        volatile struct 
         {
             uint32_t ioBase          :8;
             uint32_t ioLimit         :8;
@@ -241,115 +241,115 @@ typedef struct __attribute__((packed, aligned (4))) pcieConfigurationHeader_t
         };
 
         //type 0 config space header specific
-        uint32_t bar3;
+        volatile uint32_t bar3;
 
-        uint32_t all;
+        volatile uint32_t all;
     }dw7;
 
     //PCIe config header Dword 8 (byte offset 20h)
     union
     {
         //type 1 config space header specific
-        struct 
+        volatile struct 
         {
             uint32_t memoryBase  :16;
             uint32_t memoryLimit :16;
         };
 
         //type 0 config space header specific
-        uint32_t bar4;
+        volatile uint32_t bar4;
 
-        uint32_t all;
+        volatile uint32_t all;
     }dw8;
 
     //PCIe config header Dword 9 (byte offset 24h)
     union
     {
         //type 1 config space header specific
-        struct 
+        volatile struct 
         {
             uint32_t prefetchableMemoryBase  :16;
             uint32_t prefetchableMemoryLimit :16;
         };
 
         //type 0 config space header specific
-        uint32_t bar5;
+        volatile uint32_t bar5;
 
-        uint32_t all;
+        volatile uint32_t all;
     }dw9;
 
     //PCIe config header Dword 10 (byte offset 28h)
     union
     {
         //type 1 config space header specific
-        uint32_t prefetchableBaseUpper;
+        volatile uint32_t prefetchableBaseUpper;
 
         //type 0 config space header specific
-        uint32_t cardBusCisPtr;
+        volatile uint32_t cardBusCisPtr;
 
-        uint32_t all;
+        volatile uint32_t all;
     }dw10;
 
     //PCIe config header Dword 11 (byte offset 2Ch)
     union
     {
         //type 1 config space header specific
-        uint32_t prefetchableBaseLower;
+        volatile uint32_t prefetchableBaseLower;
 
         //type 0 config space header specific
-        struct
+        volatile struct
         {
             uint32_t subsysVendorId :16;
             uint32_t subsysDeviceId :16;
         };
 
-        uint32_t all;
+        volatile uint32_t all;
     }dw11;
     
     //PCIe config header Dword 12 (byte offset 30h)
     union
     {
         //type 1 config space header specific
-        struct
+        volatile struct
         {
             uint32_t ioBaseUpper  :16;
             uint32_t ioLimitUpper :16;
         };
 
         //type 0 config space header specific
-        uint32_t expansionRomBaseAddr;
+        volatile uint32_t expansionRomBaseAddr;
 
-        uint32_t all;
+        volatile uint32_t all;
     }dw12;
 
     //PCIe config header Dword 13 (byte offset 34h)
     union
     {
-        struct
+        volatile struct
         {
             uint32_t capPtr    :8;
             uint32_t reserved  :24;
         };
 
-        uint32_t all;
+        volatile uint32_t all;
     }dw13;
 
     //PCIe config header Dword 14 (byte offset 38h)
     union
     {
         //type 1 config space header specific
-        uint32_t expansionRomBaseAddr;
+        volatile uint32_t expansionRomBaseAddr;
 
         //type 0 config space header specific
-        uint32_t reserved;
+        volatile uint32_t reserved;
 
-        uint32_t all;
+        volatile uint32_t all;
     }dw14;
 
     //PCIe config header Dword 15 (byte offset 3Ch)
     union
     {
-        struct
+        volatile struct
         {
             uint32_t interruptLine :8;
             uint32_t interruptPin  :8;
@@ -360,7 +360,7 @@ typedef struct __attribute__((packed, aligned (4))) pcieConfigurationHeader_t
                 uint16_t bridgeControl;                
 
                 //type 0 config space header specific
-                struct
+                volatile struct
                 {
                     uint16_t minGnt :8;
                     uint16_t maxLat :8;                    
@@ -368,7 +368,7 @@ typedef struct __attribute__((packed, aligned (4))) pcieConfigurationHeader_t
             };
         };
 
-        uint32_t all;
+        volatile uint32_t all;
     }dw15;
 }
 pcieConfigurationSpaceHeader_t;
