f123e0f001f372659927f03f271470ea842ddaf6
eyang-sdgm <eyang@solidigm.com>
2026-09-24 10:52:42 -0700

    rearrange demo menu

--- test/sample/demo.cpp
+++ test/sample/demo.cpp
@@ -83,18 +83,19 @@ menu_c nvmeToolMenu_g;
 
 void demoMenu(void)
 {
-    pcieUtil_subMenu_g.addDescription("PCIe utilities");
-    nvmeUtil_subMenu_g.addDescription("NVMe utilities");
+    pcieUtil_subMenu_g.addDescription("pcie utilities");
+    nvmeUtil_subMenu_g.addDescription("nvme utilities");
 
-    tests_subMenu_g.addDescription("tests");
+    tests_subMenu_g.addDescription("demo");
     tests_subMenu_g.addItem(demo_uio, "a brief demo on uio_c driver...");
     tests_subMenu_g.addItem(demo_uio2, "bar 0 mem dump...");
     tests_subMenu_g.addItem(demo_udma, "udma_c driver");
 
-    nvmeTool_mainMenu_g.addDescription("NVMe tools 2.0");
+    nvmeTool_mainMenu_g.addDescription("NVMe Toolkit");
+    nvmeTool_mainMenu_g.addItem(tests_subMenu_g,    "demo");
     nvmeTool_mainMenu_g.addItem(pcieUtil_subMenu_g, "pcie utilities");
     nvmeTool_mainMenu_g.addItem(nvmeUtil_subMenu_g, "nvme utilities");
-    nvmeTool_mainMenu_g.addItem(tests_subMenu_g,    "tests");
+    
 
     nvmeToolMenu_g.build(nvmeTool_mainMenu_g);
     nvmeToolMenu_g.run();
